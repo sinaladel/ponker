51cc89567d295554993cfa558f08d3cdd33949d9
sinaladel <oiderem8@gmail.com>
2019-11-29 12:26:11 -0700

    handdddddd

--- app/src/main/java/com/ottt/ponker/Hand.java
+++ app/src/main/java/com/ottt/ponker/Hand.java
@@ -1,10 +1,17 @@
 package com.ottt.ponker;
 
 public class Hand implements Comparable<Hand>{
+    Player owner;
 
+    public void compareHand() {
+
+    }
 
     @Override
     public int compareTo(Hand o) {
         return 0;
     }
+
+
+
 }
